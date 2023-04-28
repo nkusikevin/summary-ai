7cbfdcc87ea370eab33c04f580601096606e06b7
nkusikevin <nkusikvnhart@gmail.com>
2023-04-28 20:14:19 +0200

    adding main styling page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import Hero from "./Components/Hero.tsx";
+import Demo from "./Components/Demo.tsx";
 
 function App() {
-	return <div>App</div>;
+	return (
+		<main className='main'>
+			<div className='gradient' />
+			<div className='app'>
+				<Hero />
+				<Demo />
+			</div>
+		</main>
+	);
 }
 
 export default App;
