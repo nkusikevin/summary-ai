9d6d26aeb5263b06fe8bd119e003c58862b00b16
nkusikevin <nkusikvnhart@gmail.com>
2023-05-01 21:47:14 +0200

    fixing deployment error

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -9,7 +9,7 @@ function Hero() {
 				<button
 					type='button'
 					onClick={() =>
-						window.open("https://github.com/TidbitsJS/Summize", "_blank")
+						window.open("https://github.com/nkusikevin/summary-ai", "_blank")
 					}
 					className='black_btn'>
 					GitHub
